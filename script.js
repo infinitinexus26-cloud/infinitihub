4494281106c3cb91d5ccc502aaf62c50e26e3e48
infinitinexus26-cloud <infinitinexus26@gmail.com>
2026-04-09 20:00:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script básico para o site
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado pelo contato! Entraremos em contato em breve.');
+});
